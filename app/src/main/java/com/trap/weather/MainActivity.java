3921967ddcae0859cdbadf300f557c5307d41926
Lostark13 <tushar9915@gmail.com>
2021-08-28 17:06:40 +0530

    save location after successfully get
    Signed-off-by: Lostark13 <tushar9915@gmail.com>

--- app/src/main/java/com/trap/weather/MainActivity.java
+++ app/src/main/java/com/trap/weather/MainActivity.java
@@ -22,15 +22,18 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
@@ -40,21 +43,21 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView city, temperature, condition, humidity, maxTemperture, minTemperature, pressure, wind, realFeel, visibilty,uvIndex,cloud,sunRise,sunSet;
+    private TextView city, temperature, condition, humidity, maxTemperture, minTemperature, pressure, wind, realFeel, visibilty, uvIndex, cloud, sunRise, sunSet;
     private ImageView iv;
     private LinearLayout linearLayout;
     private FloatingActionButton b1;
     LocationManager locationManager;
     LocationListener locationListener;
-    private String cityName="";
+    private String icode="",cityName = "";
     double lon, lat;
-
+    String filename="weather.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linearLayout=findViewById(R.id.linear_layout);
-       // linearLayout.setBackgroundResource(R.drawable.haze);
+        linearLayout = findViewById(R.id.linear_layout);
+        // linearLayout.setBackgroundResource(R.drawable.haze);
         city = findViewById(R.id.tvCity);
         temperature = findViewById(R.id.textViewTemp);
         condition = findViewById(R.id.textViewCon);
@@ -65,20 +68,21 @@ public class MainActivity extends AppCompatActivity {
         wind = findViewById(R.id.tvwind);
         realFeel = findViewById(R.id.tvreal);
         visibilty = findViewById(R.id.tvvisibilty);
-        uvIndex=findViewById(R.id.textViewUV);
-        cloud=findViewById(R.id.tvcloud);
-        sunRise=findViewById(R.id.tvsunrise);
-        sunSet=findViewById(R.id.tvsunset);
+        uvIndex = findViewById(R.id.textViewUV);
+        cloud = findViewById(R.id.tvcloud);
+        sunRise = findViewById(R.id.tvsunrise);
+        sunSet = findViewById(R.id.tvsunset);
         iv = findViewById(R.id.imageView);
         b1 = findViewById(R.id.fab);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, WeatherActivity.class);
-                i.putExtra("city",cityName);
+                i.putExtra("city", cityName);
                 startActivity(i);
             }
         });
+        getData();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
@@ -88,13 +92,13 @@ public class MainActivity extends AppCompatActivity {
                 Log.e("lat :", String.valueOf(lat));
                 Log.e("lon :", String.valueOf(lon));
                 getWeatherData(lat, lon);
-                getUVData(lat,lon);
+                getUVData(lat, lon);
             }
         };
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
-            locationManager.requestLocationUpdates(LocationManager.FUSED_PROVIDER,10000,50,locationListener);
+            locationManager.requestLocationUpdates(LocationManager.FUSED_PROVIDER, 10000, 50, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 50, locationListener);
         }
     }
@@ -116,46 +120,43 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<OpenWeather> call, Response<OpenWeather> response) {
                 city.setText(response.body().getName() + " , " + response.body().getSys().getCountry());
                 temperature.setText(((response.body().getMain().getTemp() - 273.15) + " ").substring(0, 4) + " °C");
-                visibilty.setText(": "+response.body().getVisibility() + " m");
+                visibilty.setText(": " + response.body().getVisibility() + " m");
                 condition.setText(response.body().getWeather().get(0).getDescription());
                 humidity.setText(": " + response.body().getMain().getHumidity() + " %");
                 maxTemperture.setText(": " + ((response.body().getMain().getTempMax() - 273.15) + "").substring(0, 4) + " °C");
                 minTemperature.setText(": " + ((response.body().getMain().getTempMin() - 273.15) + "").substring(0, 4) + " °C");
                 pressure.setText(": " + response.body().getMain().getPressure() + " hPa");
                 wind.setText(": " + response.body().getWind().getSpeed() + " m/s");
-                realFeel.setText(": "+((response.body().getMain().getFeelsLike() - 273.15) + " ").substring(0, 4) + " °C");
-                cloud.setText(": "+response.body().getClouds().getAll()+" %");
-                long sunrise=response.body().getSys().getSunrise();
-                long sunset=response.body().getSys().getSunset();
-                long timezone=response.body().getTimezone();
-                Date date = new Date((sunrise+timezone)*1000L);
+                realFeel.setText(": " + ((response.body().getMain().getFeelsLike() - 273.15) + " ").substring(0, 4) + " °C");
+                cloud.setText(": " + response.body().getClouds().getAll() + " %");
+                long sunrise = response.body().getSys().getSunrise();
+                long sunset = response.body().getSys().getSunset();
+                long timezone = response.body().getTimezone();
+                Date date = new Date((sunrise + timezone) * 1000L);
                 SimpleDateFormat jdf = new SimpleDateFormat("hh:mm:ss");
                 jdf.setTimeZone(TimeZone.getTimeZone("GMT+0"));
-                sunRise.setText(": "+jdf.format(date)+" AM");
-                Date date1 = new Date((sunset+timezone)*1000L);
+                sunRise.setText(": " + jdf.format(date) + " AM");
+                Date date1 = new Date((sunset + timezone) * 1000L);
                 SimpleDateFormat jdf1 = new SimpleDateFormat("hh:mm:ss");
                 jdf1.setTimeZone(TimeZone.getTimeZone("GMT+0"));
-                sunSet.setText(": "+jdf1.format(date1)+" PM");
-                cityName=response.body().getName();
-                if((condition.getText().toString()).contains("haze")){
+                sunSet.setText(": " + jdf1.format(date1) + " PM");
+                cityName = response.body().getName();
+                if ((condition.getText().toString()).contains("haze")) {
                     linearLayout.setBackgroundResource(R.drawable.haze);
-                }
-                else if((condition.getText().toString()).contains("clear")){
+                } else if ((condition.getText().toString()).contains("clear")) {
                     linearLayout.setBackgroundResource(R.drawable.clear);
-                }
-                else if((condition.getText().toString()).contains("clouds")){
+                } else if ((condition.getText().toString()).contains("clouds")) {
                     linearLayout.setBackgroundResource(R.drawable.clouds);
-                }
-                else if((condition.getText().toString()).contains("mist")){
+                } else if ((condition.getText().toString()).contains("mist")) {
                     linearLayout.setBackgroundResource(R.drawable.mist);
-                }
-                else if((condition.getText().toString()).contains("rain")){
+                } else if ((condition.getText().toString()).contains("rain")) {
                     linearLayout.setBackgroundResource(R.drawable.rain);
                 }
-                String icode = response.body().getWeather().get(0).getIcon();
+                icode = response.body().getWeather().get(0).getIcon();
                 Picasso.get().load("https://openweathermap.org/img/wn/" + icode + "@2x.png")
                         .placeholder(R.drawable.ic_launcher_background)
                         .into(iv);
+                saveData();
             }
 
             @Override
@@ -164,19 +165,74 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void getUVData(double lat, double lon) {
         WeatherAPI weatherAPI = RetrofitWeather.getclient().create(WeatherAPI.class);
         Call<OpenUV> call1 = weatherAPI.getUVWithLocation(lat, lon);
         call1.enqueue(new Callback<OpenUV>() {
             @Override
             public void onResponse(Call<OpenUV> call, Response<OpenUV> response) {
-                uvIndex.setText(": "+response.body().getValue()+"");
-                Log.e("UV",response.body().getValue()+"");
+                uvIndex.setText(": " + response.body().getValue() + "");
+                Log.e("UV", response.body().getValue() + "");
             }
             @Override
             public void onFailure(Call<OpenUV> call, Throwable t) {
                 Log.e("Error ", String.valueOf(t));
             }
         });
+    }
+    public void saveData(){
+        FileOutputStream fos;
+        String data=city.getText()+"#"+ temperature.getText()+"#"+ condition.getText()+"#" +humidity.getText()+ "#"+ maxTemperture.getText()+"#"+ minTemperature.getText()+"#"+ pressure.getText()+"#"+ wind.getText()+"#"+ realFeel.getText()+"#"+ visibilty.getText()+"#"+ uvIndex.getText()+"#"+ cloud.getText()+"#"+ sunRise.getText()+"#"+ sunSet.getText()+"#"+icode+"#";
+        try {
+            fos = openFileOutput(filename, Context.MODE_PRIVATE);
+            fos.write(data.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+    }
+    public void getData(){
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            BufferedReader inputReader = new BufferedReader(new InputStreamReader(
+                    openFileInput(filename)));
+            String inputString;
+            while ((inputString = inputReader.readLine()) != null) {
+                stringBuffer.append(inputString);
+            }
+            String arr[]=stringBuffer.toString().split("#");
+            city.setText(arr[0]);
+            temperature.setText(arr[1]);
+            condition.setText(arr[2]);
+            humidity.setText(arr[3]);
+            maxTemperture.setText(arr[4]);
+            minTemperature.setText(arr[5]);
+            pressure.setText(arr[6]);
+            wind.setText(arr[7]);
+            realFeel.setText(arr[8]);
+            visibilty.setText(arr[9]);
+            uvIndex.setText(arr[10]);
+            cloud.setText(arr[11]);
+            sunRise.setText(arr[12]);
+            sunSet.setText(arr[13]);
+            String icode=arr[14];
+            Picasso.get().load("https://openweathermap.org/img/wn/" + icode + "@2x.png")
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .into(iv);
+            if ((condition.getText().toString()).contains("haze")) {
+                linearLayout.setBackgroundResource(R.drawable.haze);
+            } else if ((condition.getText().toString()).contains("clear")) {
+                linearLayout.setBackgroundResource(R.drawable.clear);
+            } else if ((condition.getText().toString()).contains("clouds")) {
+                linearLayout.setBackgroundResource(R.drawable.clouds);
+            } else if ((condition.getText().toString()).contains("mist")) {
+                linearLayout.setBackgroundResource(R.drawable.mist);
+            } else if ((condition.getText().toString()).contains("rain")) {
+                linearLayout.setBackgroundResource(R.drawable.rain);
+            }
+            Log.e("GETDATA",stringBuffer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
